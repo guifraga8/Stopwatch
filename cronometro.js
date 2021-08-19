6d727fdae00c3ce5f254ba65b50b2af40765006f
Guilherme <gui.fraga2009@hotmail.com>
2021-08-19 03:46:43 -0300

    Indentation Correction

--- cronometro.js
+++ cronometro.js
@@ -1,28 +1,28 @@
 var intervalo;
 
 function time(op) {
-	var s = 1;
-	var m = 0;
-	intervalo = window.setInterval(function() {
-		if(s == 60){ 
+    var s = 1;
+    var m = 0;
+    intervalo = window.setInterval(function() {
+        if (s == 60) {
             m++;
             s = 0;
         }
-        if(m == 60){
+        if (m == 60) {
             s = 0;
             m = 0;
         }
-        if(s < 10){
-			document.getElementById("segundo").innerHTML = "0" + s;
-		}else{
-			document.getElementById("segundo").innerHTML = s;
-		}
-        if(m < 10){
-			document.getElementById("minuto").innerHTML = "0" + m + " :";
-		}else{
-			document.getElementById("minuto").innerHTML = m + " :";
-		}		
+        if (s < 10) {
+            document.getElementById("segundo").innerHTML = "0" + s;
+        } else {
+            document.getElementById("segundo").innerHTML = s;
+        }
+        if (m < 10) {
+            document.getElementById("minuto").innerHTML = "0" + m + " :";
+        } else {
+            document.getElementById("minuto").innerHTML = m + " :";
+        }
         s++;
-	},1000);
+    }, 1000);
 }
-window.onload=time;
+window.onload = time;
